32a74f77d192bdbcd485296251ef360f12cda6c1
bilal <basgym3@gmail.com>
2025-02-08 02:59:48 +0500

    fixed client file2

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -20,6 +20,7 @@
 // }
 
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+// env.ts
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
